0bf22c281c0618c4ed9aeb44589ae07fc7707059
kovs705 <smartkovsik@gmail.com>
2022-09-08 00:24:48 +0300

    wrote some stuff in ViewController.swift + edited and added First page with future place for WebView and embedded it with UINavigation

--- Planet/ViewController.swift
+++ Planet/ViewController.swift
@@ -5,13 +5,19 @@
 //  Created by Kovs on 07.09.2022.
 //
 
+/// First of all, as you can see, the apps name is Planet. It will use Realm dependency with CocoaPods, XCTests
+/// and other things that I will think about later (like sharing feature).
+///
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
