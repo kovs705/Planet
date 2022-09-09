53c6a01b3fabe888bad9adb5cd3fee3a01ac11f5
kovs705 <smartkovsik@gmail.com>
2022-09-09 20:53:40 +0300

    added new swift files for controllers and cells

---
+++ Planet/Cells/BookmarkCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookmarkCell.swift
+//  Planet
+//
+//  Created by Kovs on 09.09.2022.
+//
+
+import UIKit
+
+class BookmarkCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Planet/Cells/TabCell.swift
@@ -0,0 +1,23 @@
+//
+//  TabCell.swift
+//  Planet
+//
+//  Created by Kovs on 09.09.2022.
+//
+
+import UIKit
+
+class TabCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
